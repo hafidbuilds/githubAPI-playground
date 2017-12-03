617e5e50bd853f7d2e01d73e8c6139a9f2bdb900
hafidz <hafidzzpermana09@gmail.com>
2017-12-04 02:28:40 +0700

    placeholder for rendering app

---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './components/App'
+
+// import './styles/normalize.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.min.css'
+import '../node_modules/hover.css/css/hover.css'
+import './styles/components.css'
+
+ReactDOM.render(<App />, document.getElementById('root'))
