decbcf54ae6cb604e7bc45ba26c3bc5eb5a2490f
hafidz <hafidzzpermana09@gmail.com>
2017-12-15 00:53:49 +0700

    init battle pages

---
+++ client/src/components/Battle.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Battle extends React.Component {
+  render(){
+    return(
+      <div className="battle">
+        Battle
+      </div>
+      )
+  }
+}
+
+export default Battle
