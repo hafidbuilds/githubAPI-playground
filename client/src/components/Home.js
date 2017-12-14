ed4336171a458cd546ca7110b53a890afcf51de3
hafidz <hafidzzpermana09@gmail.com>
2017-12-15 00:54:21 +0700

    init home pages

---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+class Home extends React.Component {
+  render(){
+    return(
+      <div className="home">
+        <h1>Github Battle: Battle your friends... and stuff.</h1>
+        <Link className='button' to='/battle'>Battle</Link>
+      </div>
+      )
+  }
+}
+
+export default Home
